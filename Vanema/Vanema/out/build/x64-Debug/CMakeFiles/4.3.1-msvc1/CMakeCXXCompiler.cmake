743b25c5a4a76a788c0cb2243ace100d4b4e4a5e
VMDamyanova23 <VMDamyanova23@codingburgas.bg>
2026-06-15 19:27:18 +0300

    Fix built issues

--- Vanema/Vanema/out/build/x64-Debug/CMakeFiles/4.3.1-msvc1/CMakeCXXCompiler.cmake
+++ Vanema/Vanema/out/build/x64-Debug/CMakeFiles/4.3.1-msvc1/CMakeCXXCompiler.cmake
@@ -1,7 +1,7 @@
-set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/bin/Hostx64/x64/cl.exe")
+set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/18/Insiders/VC/Tools/MSVC/14.51.36231/bin/Hostx64/x64/cl.exe")
 set(CMAKE_CXX_COMPILER_ARG1 "")
 set(CMAKE_CXX_COMPILER_ID "MSVC")
-set(CMAKE_CXX_COMPILER_VERSION "19.50.35730.0")
+set(CMAKE_CXX_COMPILER_VERSION "19.51.36247.0")
 set(CMAKE_CXX_COMPILER_VERSION_INTERNAL "")
 set(CMAKE_CXX_COMPILER_WRAPPER "")
 set(CMAKE_CXX_STANDARD_COMPUTED_DEFAULT "14")
@@ -25,16 +25,17 @@ set(CMAKE_CXX_COMPILER_ARCHITECTURE_ID "x64")
 
 set(MSVC_CXX_ARCHITECTURE_ID x64)
 
-set(CMAKE_AR "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/bin/Hostx64/x64/lib.exe")
+
+set(CMAKE_AR "C:/Program Files/Microsoft Visual Studio/18/Insiders/VC/Tools/MSVC/14.51.36231/bin/Hostx64/x64/lib.exe")
 set(CMAKE_CXX_COMPILER_AR "")
 set(CMAKE_RANLIB ":")
 set(CMAKE_CXX_COMPILER_RANLIB "")
-set(CMAKE_LINKER "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/bin/Hostx64/x64/link.exe")
-set(CMAKE_LINKER_LINK "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/bin/Hostx64/x64/link.exe")
+set(CMAKE_LINKER "C:/Program Files/Microsoft Visual Studio/18/Insiders/VC/Tools/MSVC/14.51.36231/bin/Hostx64/x64/link.exe")
+set(CMAKE_LINKER_LINK "C:/Program Files/Microsoft Visual Studio/18/Insiders/VC/Tools/MSVC/14.51.36231/bin/Hostx64/x64/link.exe")
 set(CMAKE_LINKER_LLD "lld-link")
-set(CMAKE_CXX_COMPILER_LINKER "C:/PROGRA~1/MIB055~1/18/COMMUN~1/VC/Tools/MSVC/1450~1.357/bin/Hostx64/x64/link.exe")
+set(CMAKE_CXX_COMPILER_LINKER "C:/PROGRA~1/MIB055~1/18/Insiders/VC/Tools/MSVC/1451~1.362/bin/Hostx64/x64/link.exe")
 set(CMAKE_CXX_COMPILER_LINKER_ID "MSVC")
-set(CMAKE_CXX_COMPILER_LINKER_VERSION 14.50.35730.0)
+set(CMAKE_CXX_COMPILER_LINKER_VERSION 14.51.36247.0)
 set(CMAKE_CXX_COMPILER_LINKER_FRONTEND_VARIANT MSVC)
 set(CMAKE_MT "C:/Program Files (x86)/Windows Kits/10/bin/10.0.26100.0/x64/mt.exe")
 set(CMAKE_TAPI "")
@@ -97,8 +98,5 @@ set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
 set(CMAKE_CXX_COMPILER_CLANG_RESOURCE_DIR "")
 
 set(CMAKE_CXX_COMPILER_IMPORT_STD "")
-### Imported target for C++23 standard library
-set(CMAKE_CXX23_COMPILER_IMPORT_STD_NOT_FOUND_MESSAGE "Experimental `import std` support not enabled when detecting toolchain; it must be set before `CXX` is enabled (usually a `project()` call)")
-
-
-
+set(CMAKE_CXX_COMPILER_IMPORT_STD_ERROR_MESSAGE  "Experimental `import std` support not enabled when detecting toolchain; it must be set before `CXX` is enabled (usually a `project()` call)")
+set(CMAKE_CXX_STDLIB_MODULES_JSON "")
